2c6f88dc8874c00022e1f25743d6d1d93c143ec6
shahid <theshahid2001@gmail.com>
2026-05-06 14:03:12 +0530

    again cors

--- server/server.js
+++ server/server.js
@@ -15,7 +15,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://nexon-global-trading1.vercel.app'
+  ],
+  credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
